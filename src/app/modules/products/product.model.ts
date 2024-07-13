a83d45974edc3fbde73f8bff7d7c1dbc2cc90a96
Rabby <mdrabbybd4747@gmail.com>
2024-07-13 16:20:26 +0600

    added time stamps for product data

--- src/app/modules/products/product.model.ts
+++ src/app/modules/products/product.model.ts
@@ -1,13 +1,18 @@
 import { model, Schema } from 'mongoose';
 
-const productSchema = new Schema<TProduct>({
-  name: { type: String },
-  description: { type: String },
-  category: { type: String },
-  brand: { type: String },
-  quantity: { type: Number },
-  price: { type: Number },
-  image: { type: String },
-});
+const productSchema = new Schema<TProduct>(
+  {
+    name: { type: String },
+    description: { type: String },
+    category: { type: String },
+    brand: { type: String },
+    quantity: { type: Number },
+    price: { type: Number },
+    image: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export const Product = model<TProduct>('Product', productSchema);
